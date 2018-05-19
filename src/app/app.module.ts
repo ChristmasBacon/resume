dad8d0a40825519620a4458ed9df4eaac24e2eb5
jorgeomf <jorgeomf@hotmail.com>
2018-05-18 23:59:15 -0400

    Got it to work on Github!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,21 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { EducationComponent } from './education/education.component';
+import { ExperienceComponent } from './experience/experience.component';
 
 const appRoutes: Routes = [
   {path : '', component: HomeComponent},
   {path : 'Education', component: EducationComponent},
-  {path : '', component: AppComponent}
+  {path : '', component: AppComponent},
+  {path : 'Experience', component: ExperienceComponent}
 ]
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    EducationComponent
+    EducationComponent,
+    ExperienceComponent
   ],
   imports: [
     BrowserModule,
