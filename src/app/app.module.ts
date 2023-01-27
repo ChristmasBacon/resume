815a77d147af620420d3b8e4c50576fec04d3d27
ChristmasBacon <jorgeomf@hotmail.com>
2023-01-27 18:52:37 -0400

    removed all the old code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,38 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import { RouterModule, Routes } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { EducationComponent } from './education/education.component';
-import { ExperienceComponent } from './experience/experience.component';
-import { ContactComponent } from './contact/contact.component';
-import { SkillsComponent } from './skills/skills.component';
-
-const appRoutes: Routes = [
-  {path : '', component: HomeComponent},
-  {path : 'Education', component: EducationComponent},
-  {path : '', component: AppComponent},
-  {path : 'Experience', component: ExperienceComponent},
-  {path : 'Contact', component: ContactComponent},
-  {path : 'Skills', component: SkillsComponent}
-]
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    EducationComponent,
-    ExperienceComponent,
-    ContactComponent,
-    SkillsComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(
-      appRoutes,
-      { enableTracing: true } // <-- debugging purposes only
-    )
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
