5a258b239097cd240ff508adb248e1beb30f0026
ChristmasBacon <jorgeomf@hotmail.com>
2023-02-25 23:22:11 -0400

    added multiple components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ExperienceComponent } from './experience/experience.component';
+import { SkillsComponent } from './skills/skills.component';
+import { SummaryComponent } from './summary/summary.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'experience', component: ExperienceComponent},
+  { path: 'skills', component: SkillsComponent},
+  { path: 'summary', component: SummaryComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
